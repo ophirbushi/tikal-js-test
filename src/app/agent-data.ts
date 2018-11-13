ef3eec76efd1c492bdacb3620edb39e872501de7
Ophir Bushinsky <ophirbushi@gmail.com>
2018-11-13 16:15:17 +0200

    first version

---
+++ src/app/agent-data.ts
@@ -0,0 +1,6 @@
+export interface AgentData {
+  agent: string;
+  country: string;
+  address: string;
+  date: string;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AgentData } from './agent-data';
+import { SAMPLE_DATA } from './sample-data';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'agent-m16';
+
+  readonly data: AgentData[] = SAMPLE_DATA;
+  mostIsolatedCountry: string;
+
+  findMostIsolatedCountry(data: AgentData[]): string {
+    const groupsDictionary: CountryAgentsDictionary = {};
+
+    data.forEach(agentData => {
+      if (!groupsDictionary[agentData.country]) {
+        groupsDictionary[agentData.country] = [];
+      }
+      groupsDictionary[agentData.country].push(agentData.agent);
+    });
+
+    const sorted = Object.keys(groupsDictionary)
+      .map(key => ({ country: key, agentsCount: groupsDictionary[key].length }))
+      .sort((a, b) => b.agentsCount - a.agentsCount)
+      .map(p => p.country);
+
+    return sorted[0];
+  }
+
+  onFindIsolatedButtonClick() {
+    this.mostIsolatedCountry = this.findMostIsolatedCountry(this.data);
+  }
+}
+
+interface CountryAgentsDictionary {
+  [country: string]: string[];
 }
